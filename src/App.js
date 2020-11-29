697c5b8be3dfa46f3078c5bbcd4a12218199248d
Leandro Henriques <lehenriques@gmail.com>
2020-11-28 21:53:09 -0300

    Arrumando codigo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import {
   SafeAreaView,
@@ -9,63 +11,64 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
-import api from "./services/api";
 
 export default function App() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepository] = useState([]);
 
-  useEffect( () => {
-    api.get('repositories').then( response => {
-      setRepositories(response.data);
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepository(response.data);
     });
   }, []);
 
   async function handleLikeRepository(id) {
-    const response = await api.post(`repositories/${id}/like`);
-
-    const likedRepository = response.data;
-
-    const repositoriesUpdated = repositories.map(repositoy => {
-      if (repository.id === id ){
-        return likedRepository;
-      }else{
-        return repositoy;
-      }
+    const response = await api.post(`repositories/${id}/like`, {
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
     });
 
-    setRepositories(repositoriesUpdated);
+    const repository = response.data;
+
+    const repositoryIndex = repositories.findIndex(repo => repo.id === repository.id);
+
+    // setRepository([...repositories.filter(repository => repository.id !== id), repository]);
+    setRepository([...repositories.slice(0, repositoryIndex), repository, ...repositories.slice(repositoryIndex + 1)]);
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        
         <FlatList
           data={repositories}
           keyExtractor={repository => repository.id}
-          renderItem={ ({item: repository}) => (
+          renderItem={({ item: repository }) => (
             <View style={styles.repositoryContainer}>
               <Text style={styles.repository}>{repository.title}</Text>
 
-              <View style={styles.TechsContainer}>
-                {repository.techs.map(tech => {
-                  <Text key={tech} style={styles.tech}>
-                    {tech}
-                  </Text>
-                })}
+              <View style={styles.techsContainer}>
+                <Text style={styles.tech}>
+                  ReactJS
+                </Text>
+                <Text style={styles.tech}>
+                  Node.js
+                </Text>
               </View>
 
               <View style={styles.likesContainer}>
-                <Text style={styles.likeText} textID={`repository-likes-${repository.id}`}>
+                <Text
+                  style={styles.likeText}
+                  testID={`repository-likes-${repository.id}`}
+                >
                   {repository.likes} curtidas
                 </Text>
               </View>
 
               <TouchableOpacity
-              style={styles.button}
-              onPress={() => handleLikeRepository(repository.id)}
-              testID={`like-button-${repository.id}`}
+                style={styles.button}
+                onPress={() => handleLikeRepository(repository.id)}
+                testID={`like-button-${repository.id}`}
               >
                 <Text style={styles.buttonText}>Curtir</Text>
               </TouchableOpacity>
